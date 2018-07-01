f24c9ab9715354c9b3c8859737477286cf5a3386
Unknown <2007mjm@gmail.com>
2018-07-01 18:34:32 +0430

    change api url

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,7 +1,7 @@
 const SERVER_DOMAIN = 'ibapp.ir'
 const PROTO = 'http'
 
-export const API_URL = `${PROTO}://${SERVER_DOMAIN}:1312/`
+export const API_URL = `${PROTO}://${SERVER_DOMAIN}:1312/api/v1/`
 // export const WS_URL = `ws://${SERVER_DOMAIN}:3005`
 // export const FILES_BASE_URL = `${PROTO}://${SERVER_DOMAIN}:3001/`
 
